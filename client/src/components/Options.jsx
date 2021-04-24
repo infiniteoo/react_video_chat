8a22b2c3e161872f071a3672ce79780db6e6f70f
Troy Dorman <troydorman@gmail.com>
2021-04-24 08:15:53 -0600

    finished the options component where we define our name and enter the ID of ppl to call

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -1,5 +1,11 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
+import React, { useContext, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, TextField, Grid, Typography, Container, Paper } from '@material-ui/core';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Assignment, Phone, PhoneDisabled } from '@material-ui/icons';
+
+import { SocketContext } from './SocketContext';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -36,13 +42,59 @@ const useStyles = makeStyles((theme) => ({
 const Options = ( { children } ) => {
 
     const classes = useStyles();
-
+    const {me, callAccepted, name, setName, callEnded, leaveCall, callUser} = useContext(SocketContext);
+    const [idToCall, setIdToCall] = useState("");
+    
 
     return (
-        <div>
+       <Container className={classes.container}>
+           <Paper elevation={10} className={classes.paper}>
+               <form className={classes.root} noValidate autoComplete="off">
+                   <Grid container className={classes.gridContainer}>
+                       <Grid item xs={12} md={6} className={classes.padding}>
+                           <Typography gutterBottom variant="h6">
+                                Account Info
+                           </Typography>
+                           <TextField label="Name" value={name} onChange={(e) => setName(e.target.value)} fullWidth/>
+                           <CopyToClipboard text={me} className={classes.margin}>
+                               <Button variant="contained" color="primary" fullWidth startIcon={<Assignment fontSize="large"/>}/>
+
+                           </CopyToClipboard>
+                       </Grid>
+                       <Grid item xs={12} md={6} className={classes.padding}>
+                           <Typography gutterBottom variant="h6">
+                               Make a Call
+                           </Typography>
+                           <TextField label="ID to Call" value={idToCall} onChange={(e) => setIdToCall(e.target.value)} fullWidth/>
+                           {callAccepted && !callEnded ? (
+                            <Button 
+                            variant="contained" 
+                            color="secondary" 
+                            startIcon={<PhoneDisabled fontSize="large"/>}
+                            fullWidth
+                            onClick={leaveCall}
+                            className={classes.margin}
+                            >Hang Up</Button>
+                           ) : (
+                            <Button
+                            variant="contained" 
+                            color="secondary" 
+                            startIcon={<Phone fontSize="large"/>}
+                            fullWidth
+                            onClick={() => callUser(idToCall)}
+                            className={classes.margin}
+                            >Call </Button>
+                           )
+                           }
+                       </Grid>
+                   </Grid>
+               </form>
+           </Paper>
             Options
             {children}
-        </div>
+       </Container>
+           
+        
     )
 }
 
